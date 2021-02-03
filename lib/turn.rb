05fc68565050388069b9b2bade58df108de9e115
Medhanei  Abraha <abrahama2019@gmail.com>
2021-02-03 06:29:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,3 +20,18 @@ end
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
+
+def move(board, index, token="X")
+  board[index] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move(board, index)
+    move(board, index)
+    display_board(board)
+  else 
+    puts "NOT A VALID MOVE"
+    turn(board)
